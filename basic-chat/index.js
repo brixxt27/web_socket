36dc995da7dd0b51a87bb0d39cda62c5c2cdee36
brixxt27 <brixxt27@gmail.com>
2024-07-24 18:52:40 +0900

    서버 room 적용

--- basic-chat/index.js
+++ basic-chat/index.js
@@ -1,16 +1,23 @@
-const express = require("express")
+const express = require('express')
 const app = express()
-const http = require("http")
-const { Server } = require("socket.io")
-const path = require("path")
+const http = require('http')
+const { Server } = require('socket.io')
+const path = require('path')
 const server = http.createServer(app)
-const io = new Server(server)
+const serverSocket = new Server(server, {
+  connectionStateRecovery: {
+    timeout: 5000,
+    maxDisconnectionDuration: 60000,
+  },
+})
+
+const chatRoom = '1'
 
 /**
  * public 디렉토리에 있는 정적 파일을 제공합니다.
  * js 파일을 HTML 파일에서 불러올 수 있도록 합니다.
  */
-app.use(express.static(path.join(__dirname, "public")))
+app.use(express.static(path.join(__dirname, 'public')))
 
 class MessageObj {
   id
@@ -23,46 +30,58 @@ class MessageObj {
   }
 }
 
-const users = new Map()
-
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"))
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'))
 })
 
 /**
  * 채팅방에 입장하면 해당 소켓의 모든 유저에게 입장 메시지를 보냅니다.
  */
-io.on("connection", (socket) => {
-  io.emit("enter", new MessageObj("system", "채팅방에 입장하셨습니다."))
+serverSocket.on('connection', (clientSocket) => {
+  clientSocket.join(chatRoom)
+  serverSocket
+    .in(chatRoom)
+    .emit('enter', new MessageObj('system', '채팅방에 입장하셨습니다.'))
+  // serverSocket.emit(
+  //   'enter',
+  //   new MessageObj('system', '채팅방에 입장하셨습니다.'),
+  // )
 
   /**
    * 채팅 메시지를 받으면  해당 소켓의 모든 유저에게 메시지를 보냅니다.
    */
-  socket.on("message", (obj) => {
+  clientSocket.on('message', (obj) => {
     const { nickname, message } = obj
     console.log(`${nickname}: ` + message)
-    socket.broadcast.emit("chat message", obj)
+    clientSocket.in(chatRoom).emit('message', obj)
+    // clientSocket.broadcast.emit('message', obj)
   })
 
   /**
    * 유저가 타이핑 중이라면 자신을 제외한 소켓에 연결된 모든 유저에게 타이핑 중이라는 메시지를 보냅니다.
    */
-  socket.on("typing", (nickname) => {
-    socket.broadcast.emit("typing", nickname + "님이 입력 중입니다.")
+  clientSocket.on('typing', (nickname) => {
+    clientSocket.in(chatRoom).emit('typing', nickname + '님이 입력 중입니다.')
+    // clientSocket.broadcast.emit('typing', nickname + '님이 입력 중입니다.')
   })
 
-  socket.on("stopTyping", () => {
-    socket.broadcast.emit("stopTyping")
+  clientSocket.on('stopTyping', () => {
+    clientSocket.in(chatRoom).emit('stopTyping')
+    // clientSocket.broadcast.emit('stopTyping')
   })
 
   /**
    * 채팅방에서 나가면 자신을 제외한 소켓에 연결된 모든 유저에게 나갔다는 메시지를 보냅니다.
    */
-  socket.on("disconnect", () => {
-    socket.broadcast.emit(
-      "exit",
-      new MessageObj("system", "채팅방에서 나가셨습니다.")
-    )
+  clientSocket.on('disconnect', () => {
+    clientSocket.leave(chatRoom)
+    clientSocket
+      .in(chatRoom)
+      .emit('exit', new MessageObj('system', '채팅방에서 나가셨습니다.'))
+    // clientSocket.broadcast.emit(
+    //   'exit',
+    //   new MessageObj('system', '채팅방에서 나가셨습니다.'),
+    // )
   })
 })
 
@@ -70,5 +89,5 @@ io.on("connection", (socket) => {
  * 3000 포트로 litening하도록 서버를 실행합니다.
  */
 server.listen(3000, () => {
-  console.log("Listening on *:3000")
+  console.log('Listening on *:3000')
 })
